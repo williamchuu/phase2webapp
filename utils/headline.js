ceefacf60cdd4245d099c51a2931e7776407d125
pho86@my.bcit.ca <pho86@my.bcit.ca>
2022-02-19 15:19:26 -0800

    badges/headline components mostly done
    Currently badge components are flipped through a hover effect, as dont know how to make w/ javascript. Also, missing some svgs.

---
+++ utils/headline.js
@@ -0,0 +1,52 @@
+//MUST HAVE - CREATE A TEMPLATE TAG
+var template_headline = document.createElement("template"); //<template> </template> RULE
+
+//To-do - CREATE THE UI HERE!
+template_headline.innerHTML = `
+<style>
+    .headline {
+        display:flex;
+        align-items:center;
+        flex-direction: column;
+        width: 20rem;
+    }
+    .headline img {
+        width: 20rem;
+    }
+    .headline_text {
+        font-weight: bold;
+        font-size:1.2rem;
+    }
+
+</style>
+<div class="headline">
+    <img src="img/boy.svg">
+    <div class ="headline_text">
+    <h1>Hello World</h1>
+    </div>
+
+</div>
+`;
+
+//MUST HAVE - CREATE A CLASS WITH HTMLELEMENT POWERS (interfaces/functionalities)
+class TheHeadline extends HTMLElement {
+
+    //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
+    constructor(){
+        super(); //pass on the HTMLElement super powers!
+        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+
+        //To-do - CREATE THE STATES FOR THE UI HERE!
+    }
+
+    //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
+    connectedCallback(){
+        this.shadowRoot.appendChild(template_headline.content.cloneNode(true)); //use the template to make a clone
+        this.shadowRoot.querySelector(".headline_text").innerHTML = this.getAttribute("head_text");
+    }
+
+    //To-do - CREATE THE FUNCTIONALITIES HERE!
+}
+
+//MUST HAVE - define the tag for the custom elements
+customElements.define("the-headline", TheHeadline)
