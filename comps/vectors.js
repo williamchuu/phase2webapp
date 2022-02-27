43b58933fd4637f3eb463c6c34c7a7cb6a5950dd
William <wchu41@my.bcit.ca>
2022-02-26 20:06:15 -0800

    added the other vector icons

--- comps/vectors.js
+++ comps/vectors.js
@@ -15,7 +15,7 @@ template_vector.innerHTML = `
 
 <div class="vectors">
     <div class="icon">
-     <img src="img/trash.svg">
+     <img src="img/salad.svg">
     </div
 </div>
 `;
@@ -24,28 +24,54 @@ template_vector.innerHTML = `
 class TheVector extends HTMLElement {
 
     //MUST HAVE - CREATE A CONSTRUCTOR TO DO INITAL ASSOCIATIONS
-    constructor(){
+    constructor() {
         super(); //pass on the HTMLElement super powers!
-        this.attachShadow({mode:"open"}) //Attach it to the shadowRoot
+        this.attachShadow({ mode: "open" }) //Attach it to the shadowRoot
 
         //To-do - CREATE THE STATES FOR THE UI HERE!
     }
 
     //MUST HAVE - FUNCTION THAT RUNS AFTER IT'S CONNECTED
-    connectedCallback(){
+    connectedCallback() {
         this.shadowRoot.appendChild(template_vector.content.cloneNode(true)); //use the template to make a clone
-        this.shadowRoot.querySelector("#icon").innerText = this.getAttribute("icon");
-        if(this.getAttribute(".icon")){
-            document.querySelector(".icon").changeIcon(`./imgs/${this.getAttribute("icon")}.svg`);
+ 
+        if (this.getAttribute("pic") === "jar") {
+            document.querySelector(".icons").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "salad") {
+            document.querySelector(".icons").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "landfill") {
+            document.querySelector(".icons").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "restaurant") {
+            document.querySelector(".icons").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        } 
+        // aaaaaaaaaaaaaaaaaaa
 
+        
+        if (this.getAttribute("pic") === "jar") {
+            document.querySelector(".jaricon").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "salad") {
+            document.querySelector(".saladicon").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "landfill") {
+            document.querySelector(".landfillicon").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "restaurant") {
+            document.querySelector(".restauranticon").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
+        }
+        else if (this.getAttribute("pic") === "trash") {
+            document.querySelector(".trashicon").changeIcon(`./img/${this.getAttribute("pic")}.svg`);
         }
     }
 
     //To-do - CREATE THE FUNCTIONALITIES HERE!
-    changeIcon(img='./img/salad.svg'){
-        this.shadowRoot.querySelector("#vectors").src = img;
-        
+    changeIcon(img = './img/trash.svg') {
+        this.shadowRoot.querySelector(".icon > img").src = img;
     }
+
 }
 
 //MUST HAVE - define the tag for the custom elements
